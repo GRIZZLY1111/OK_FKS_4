dc4eb9fe9ae1e411c197ff2ae6164cf4ebc73c58
Ваше Имя <_@example.com>
2026-03-11 14:14:43 +0500

    First

---
+++ GeometryShape/GeometryShape/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        private double _width;
+        public double Width 
+        {
+            get => _width;
+            set{
+                if (_width != value)
+                {
+                    _width = value;
+                }
+            }
+        }
+        private double _height;
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                }
+            }
+        }
+        public override double Area()
+        {
+            double area = Width * Height;
+            return area;
+        }
+        public override double Perimeter()
+        {
+            double perimeter = Width * 2 + Height * 2;
+            return perimeter;
+        }
+        public override string ToString()
+        {
+            string str = $"Это прямоугольник и его ширина равна {Width}, его высота равна {Height}.\n" +
+                $"Его площадь равна {Area()}, а его периметр равен {Perimeter()}";
+            return str;
+        }
+    }
+}
---
+++ GeometryShape/GeometryShape/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle : Shape
+    {
+        public double SideA;
+        public double SideB;
+        public double SideC;
+        public override double Area()
+        {
+            double poluPerimeter = (SideA + SideB + SideC)/2;
+            double area = Math.Sqrt((poluPerimeter*(poluPerimeter-SideA)* (poluPerimeter - SideB)* (poluPerimeter - SideC)));
+            return area;
+        }
+        public override double Perimeter()
+        {
+            double perimeter = SideA + SideB + SideC;
+            return perimeter;
+        }
+        public override string ToString()
+        {
+            string str =  $"Это треугольник и его первая сторона равна {SideA}, его вторая сторона равна {SideB} " +
+                $"и его третья сторона равна {SideC}. \n" +
+                $"Его площадь равна {Area()}, а его периметр равен{Perimeter()}";
+            return str;
+        }
+    }
+}
